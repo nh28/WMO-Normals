f2ccf8cf06d68a06abe26864192305a198ac1411
Harsch <Nena.Harsch@ec.gc.ca>
2024-01-24 10:34:11 -0500

    filling station header works

--- src/StationList.py
+++ src/StationList.py
@@ -1,23 +1,32 @@
 class StationList:
-    station_indices_dict = {} 
+    name_key = {"WMO_Number": ["WMO-ID", 5] , "Latitude": ["Latitude", 11], "Longitude": ["Longitude", 12], "Station_Height": ["Elevation (m)", 13],
+                "WMO Integrated Global Observing System (WIGOS) Station Identifier (if available)": ["WIGOS-ID", 6]}
+    dim_row = 13
+    dim_col = 3
 
     def __init__(self, template, station_list, station_name=None):
         self.template = template
         self.station_list = station_list
         self.station_name = station_name if station_name is not None else ""
 
-    def fill_station_indices(self, dim_row, dim_col):
-        self.station_list
-        # make this a key: [parameterName, value: [col index in station list, row, col] or [] if not
-        #hard to do since the station list headers do not match the template names
-        #create another file? extend to a previous file?
+    def add_names_key(self, list):
+        split_list = list.split(':')
+        self.names_key[split_list[0]] = [split_list[1], split_list[2]]
 
-        #just precode it likely not to change 
-        # round the station height to the nearest whole number 
-        # elevation vs baramoeter elevation
+    def change_dim_row(self, row):
+        self.dim_row = row
+    
+    def change_dim_col(self, col):
+        self.dim_col = col
+
+    def find_in_temp(self, key):
+        for r in range(self.dim_row + 1):
+            for c in range(self.dim_col + 1):
+                if self.template.iloc[r, c] == key:
+                    return [r, c]
+        
 
     def find_station_row(self):
-        print(len(self.station_list.value_counts()))
         num = 0;
         while (self.station_list.iloc[num, 1] is not None):
             if self.station_name == self.station_list.iloc[num, 1]:
@@ -26,25 +35,29 @@ class StationList:
         return -1
     
     def fill_station_data(self):
-        self.template.at[5, 1] = "CANADA"
-        self.template.at[6, 1] = self.station_name
+        self.template.iloc[4, 1] = "CANADA"
+        self.template.iloc[5, 1] = self.station_name
         
         wmo = ""
         row = self.find_station_row()
-        for key, value in StationList.station_indices_dict.items():
-            if value.size() != 0: #later add if the station does not exist
-                value = self.station_list.loc[row, StationList.station_indices_dict[key][0]]
-                print(value)
-                if (key == "Latitude" or key == "Longitude"):
-                    translation_table = str.maketrans({"'": "|", '"': "|", "°": "|"})
-                    value = value.translate(translation_table)
+        for key, value in self.name_key.items():
+            loca = self.find_in_temp(key)            
+            data = str(self.station_list.iloc[row, value[1]])
 
-                self.template.loc[StationList.station_indices_dict[key][1] + 1, 
-                                  StationList.station_indices_dict[key][2]] = value
+            #rounding
+            index = data.find(".")
+            if (index != -1):
+                if int(data[(index + 1):]) >= 5:
+                    data = int(data[:index]) + 1
+                else:
+                    data = data[:index] 
                 
-                if (key == "WMO-ID"):
-                    wmo = value
-
+            if (key == "Latitude" or key == "Longitude"):
+                translation_table = str.maketrans({"'": "|", '"': "|", "°": "|"})
+                data = data.translate(translation_table)
+            self.template.iloc[loca[0] + 1, loca[1]] = data       
+            if (key == "WMO_Number"):
+                wmo = data
         return wmo
              
                     
--- src/WMO.py
+++ src/WMO.py
@@ -11,19 +11,31 @@ def main():
     template_path = r"C:\Users\harschn\Documents\PortableGit\WMO-Normals\resources\Copy of WMO_Normals_csv_Template_primary_secondary.csv"
     #input("Please enter the path to the WMO template")
     template_df = pd.read_csv(template_path)
-    dim_row = 13 # input("Please indicate what row the station header ends")
-    dim_col = 3 #input("Please indicate what col the station header ends")
 
     station_list_path = r"C:\Users\harschn\Documents\PortableGit\WMO-Normals\resources\StationList.csv"
     #input("Please enter the Station List")
     station_list_df = pd.read_csv(station_list_path)
+    gen_station = StationList(template_df, station_list_df, "")
+    bool = ""
+    while (bool != "quit"):
+        station_key = input("Please enter any other information you need to input for a station (NOT Name, Country, WMO-ID, WIGOS-ID, Lat, Long, Elevation)" + 
+                            "\nEnter it in the format template_name:station_list_name:column_on_station_list" + 
+                            "\nTo exit, enter quit\n")
+        if (station_key.lower() == "quit"):
+            bool = station_key.lower()
+        else:
+            gen_station.add_names_key(station_key)
+            dim_row = input("Please indicate what row the station header ends")
+            gen_station.change_dim_row(dim_row)
+            dim_col = input("Please indicate what col the station header ends")
+            gen_station.change_dim_col(dim_col)
+        
 
     normal_parameters_path = r"C:\Users\harschn\Documents\PortableGit\WMO-Normals\resources\NormalID_to_WMOParameterID.csv"
     #input("Please enter the path to the Normal ID to WMO Parameter ID")
     normals_parameters_df = pd.read_csv(normal_parameters_path)
 
-    gen_station = StationList(template_df, station_list_df,"")
-    gen_station.fill_station_indices(dim_row, dim_col)
+  
 
     all_stations = wmo_data_set_df.groupby('ENG_STN_NAME')
 
@@ -31,8 +43,7 @@ def main():
         # fill the information about the station
         station_temp_df = template_df.copy()
         one_station = StationList(station_temp_df, station_list_df, station_name)
-        # wmo_id = one_station.fill_station_data() #might need to return the data frame instead
-        # print(wmo_id)
+        wmo_id = one_station.fill_station_data() #might need to return the data frame insteadquitqui
 
         """el = Elements(station_temp_df, normals_parameters_df)
         all_elements = station_df.groupby("NORMAL_ID")
